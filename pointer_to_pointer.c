d1f3aa7256c1329f5fdf5c041a04563ecc7eeac1
Ravikanth <ravikanthapple489@gmail.com>
2026-05-15 17:19:01 +0530

    updated double pointers

--- pointer_to_pointer.c
+++ pointer_to_pointer.c
@@ -6,6 +6,10 @@ int main(){
     int a = 10;     // type of a is int
     int *p = &a;    // type of p is int*
     int **q= &p;    // q points to p and the type of q is int**
+    int** r = &q;
+    printf("Dereferencing pointer p %d\n",*p);
+    printf("Dereferencing pointer q which store address of p %d\n",*q);
+    printf("De-Dereferencing pointer q which gives the value of a %d\n",*(*q));
     printf("%p\n",q);
-    printf("%p",&p);
+    printf("%p\n",&p);
 }
