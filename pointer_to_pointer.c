aa697dcc9f7cd2957b1fdb8c19005f76a75bbc23
Ravikanth <ravikanthapple489@gmail.com>
2026-05-15 17:24:33 +0530

    updated double and triple pointers clearly

--- pointer_to_pointer.c
+++ pointer_to_pointer.c
@@ -6,10 +6,16 @@ int main(){
     int a = 10;     // type of a is int
     int *p = &a;    // type of p is int*
     int **q= &p;    // q points to p and the type of q is int**
-    int** r = &q;
+    int*** r = &q;
     printf("Dereferencing pointer p %d\n",*p);
     printf("Dereferencing pointer q which store address of p %d\n",*q);
     printf("De-Dereferencing pointer q which gives the value of a %d\n",*(*q));
     printf("%p\n",q);
     printf("%p\n",&p);
+
+    ***r = 20;
+    printf("%d\n",a);
+
+    **q = *p + 2;    // the ouput will be 22
+    printf("%d\n",a);
 }
